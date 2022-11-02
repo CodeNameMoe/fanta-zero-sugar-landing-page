bb920f07df1e18970802c8b9f5c8647250fc487e
Mohamed Ali <newmoeali@gmail.com>
2022-11-02 21:43:30 +0000

    completed js logic for img slider and colour changer

---
+++ app.js
@@ -0,0 +1,11 @@
+function imgSlider(any){
+    document.querySelector(".fanta").src = any;
+
+}
+
+function changeColour(colour){
+    const circle = document.querySelector(".circle");
+
+    circle.style.background = colour
+
+}

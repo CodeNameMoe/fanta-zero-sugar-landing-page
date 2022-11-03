f3eabe5999937dbd72632e4a5c93e365b62b02cd
Mohamed Ali <newmoeali@gmail.com>
2022-11-03 14:49:14 +0000

    added media queries for mobile responsiveness & responsive toggle navbar

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ function changeColour(colour){
     circle.style.background = colour
 
 }
+
+function toggleMenu(){
+    let menuToggle = document.querySelector(".toggle")
+    let navigation = document.querySelector(".navigation")
+    menuToggle.classList.toggle("active");
+    navigation.classList.toggle("active")
+}

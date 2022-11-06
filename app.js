8b7fcd818d16cec033946fc198831d2b4e846954
Mohamed Ali <newmoeali@gmail.com>
2022-11-06 18:16:08 +0000

    added can switcher section markup

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-function imgSlider(any){
+function imgSlider(any) {
     document.querySelector(".fanta").src = any;
 
 }
 
-function changeColour(colour){
+function changeColour(colour) {
     const circle = document.querySelector(".circle");
 
     circle.style.background = colour
 
 }
 
-function toggleMenu(){
+function toggleMenu() {
     let menuToggle = document.querySelector(".toggle")
     let navigation = document.querySelector(".navigation")
     menuToggle.classList.toggle("active");
     navigation.classList.toggle("active")
+}
+
+function mobileToggle() {
+    let src = "";
+
+    let canArr = ["/assets/2-p.png", "/assets/3-o.png", "/assets/4-r.png"]
+
+    let rNum = Math.floor(Math.random() * 3)
+
+    return canArr[rNum]
 }
